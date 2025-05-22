d3b3d13bebd5a91f97d0e82aafb2de94e20d608c
KoiX1024 <bgzxz1987@gmail.com>
2025-05-22 21:57:08 +0800

    jdk 8

--- jdk-feature-showcase/src/main/java/io/example/jdk8/FunctionalInterfaceExample.java
+++
@@ -1,8 +0,0 @@
-package io.example.jdk8;
-
-public class FunctionalInterfaceExample {
-    public static void main(String[] args) {
-        GreetingService greetService1 = message -> System.out.println("Hello " + message);
-        greetService1.sayMessage("World");
-    }
-}
--- jdk-feature-showcase/src/main/java/io/example/jdk8/GreetingService.java
+++
@@ -1,8 +0,0 @@
-package io.example.jdk8;
-
-import java.lang.FunctionalInterface;
-
-@FunctionalInterface
-public interface GreetingService {
-    void sayMessage(String message);
-}
